d302d31cf3d63e92ad07f706a7bcfe04bbc13269
张文亮 <1312480826@qq.com>
2020-12-06 11:14:05 +0800

    完善职位管理页面的显示和完成与后端职位管理接口的对接

--- src/components/sys/basic/PosManange.vue
+++ src/components/sys/basic/PosManange.vue
@@ -6,9 +6,10 @@
                     class="addPosInput"
                     placeholder="添加职位..."
                     prefix-icon="el-icon-plus"
+                    @keydown.enter.native="addPosition"
                     v-model="pos.name">
             </el-input>
-            <el-button icon="el-icon-plus" size="small" type="primary">添加</el-button>
+            <el-button icon="el-icon-plus" size="small" type="primary" @click="addPosition">添加</el-button>
         </div>
         <div class="posManageTable">
             <el-table
@@ -17,6 +18,10 @@
                     size="small"
                     stripe
                     style="width: 80%">
+                <el-table-column
+                        type="selection"
+                        width="55">
+                </el-table-column>
                 <el-table-column
                         prop="id"
                         label="编号"
@@ -28,8 +33,20 @@
                         width="120">
                 </el-table-column>
                 <el-table-column
-                        prop="createdate"
+                        prop="createDate"
                         label="创建时间">
+                </el-table-column>
+                <el-table-column
+                        label="操作">
+                    <template slot-scope="scope">
+                        <el-button
+                                size="mini"
+                                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <el-button
+                                size="mini"
+                                type="danger"
+                                @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                    </template>
                 </el-table-column>
             </el-table>
         </div>
@@ -45,6 +62,51 @@
                     name: ''
                 },
                 positions: []
+            }
+        },
+        mounted() {
+            this.initPosition();
+        },
+        methods: {
+            initPosition() {
+                this.getRequest("/system/basic/pos/").then(resp =>{
+                    if(resp) {
+                        this.positions = resp;
+                    }
+                })
+            },
+            handleEdit(index, data) {
+
+            },
+            handleDelete(index, data){
+                this.$confirm('是否删除【'+ data.name + '】职位? ', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequest("/system/basic/pos/" + data.id).then(resp=>{
+                        if(resp){
+                            this.initPosition();
+                        }
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            addPosition(){
+                if(this.pos.name){
+                    this.postRequest("/system/basic/pos/", this.pos).then(resp =>{
+                        if(resp){
+                            this.initPosition();
+                            this.pos.name = "";
+                        }
+                    })
+                }else {
+                    this.$message.error('职位名称不能为空！');
+                }
             }
         }
     }
