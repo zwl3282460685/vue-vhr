e2558b4b503a53bce3aa6efd058ebd91d94190ab
张文亮 <1312480826@qq.com>
2020-12-08 22:46:40 +0800

    部门管理页的实现和与后端接口的对接

--- src/components/sys/basic/DepManage.vue
+++ src/components/sys/basic/DepManage.vue
@@ -1,15 +1,174 @@
 <template>
-    <div>
-        部门管理
+    <div style="width: 500px">
+        <el-input
+                prefix-icon="el-icon-search"
+                placeholder="输入部门名称进行搜索..."
+                v-model="filterText">
+        </el-input>
+
+        <el-tree
+                style="margin-top: 5px"
+                :data="dps"
+                :props="defaultProps"
+                :expand-on-click-node="false"
+                :filter-node-method="filterNode"
+                ref="tree">
+            <span class="custom-tree-node" slot-scope="{ node, data }" style="display: flex; justify-content: space-between; width: 100%">
+                <span>{{ node.label }}</span>
+                <span>
+                  <el-button
+                          type="primary"
+                          size="mini"
+                          class="depBtn"
+                          @click="() => showAddView(data)">
+                    添加部门
+                  </el-button>
+                  <el-button
+                          type="danger"
+                          size="mini"
+                          class="depBtn"
+                          @click="() => deleteDep(data)">
+                    删除部门
+                  </el-button>
+                </span>
+          </span>
+        </el-tree>
+        <el-dialog
+                title="添加部门"
+                :visible.sync="dialogVisible"
+                width="30%">
+            <div>
+                <table>
+                    <tr>
+                        <td><el-tag>上级部门</el-tag></td>
+                        <td><span>{{pname}}</span></td>
+                    </tr>
+                    <tr>
+                        <td><el-tag>部门名称</el-tag></td>
+                        <td><el-input size="small" v-model="dep.name" placeholder="请输入部门名称..."></el-input></td>
+                    </tr>
+                </table>
+            </div>
+            <span slot="footer" class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="doAddDep">确 定</el-button>
+              </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
     export default {
-        name: "DepManage"
+        name: "DepManage",
+        data() {
+            return {
+                dialogVisible: false,
+                dep: {
+                    name: '',
+                    parentId: -1
+                },
+                pname: '',
+                filterText: '',
+                dps: [],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                }
+            }
+        },
+        watch: {
+            filterText(val) {
+                this.$refs.tree.filter(val);
+            }
+        },
+        mounted() {
+            this.initDps();
+        },
+        methods: {
+            removeDepFromDeps(deps, id){
+                for(let i =0; i < deps.length; i++){
+                    let d = deps[i];
+                    if(d.id == id){
+                        deps.splice(i, 1);
+                        return;
+                    }else{
+                        this.removeDepFromDeps(d.children, id);
+                    }
+                }
+            },
+            addDep2Deps(deps, dep){
+                for(let i = 0; i < deps.length; i ++){
+                    let d = deps[i];
+                    if(d.id == dep.parentId){
+                        d.children=d.children.concat(dep);
+                        return;
+                    }else{
+                        this.addDep2Deps(d.children, dep);
+                    }
+                }
+            },
+            initDep(){
+              this.dep={
+                  name: '',
+                  parentId: -1
+              }
+              this.pname = '';
+            },
+            doAddDep(){
+                this.postRequest("/system/basic/department/", this.dep).then(resp=>{
+                    if(resp){
+                        this.addDep2Deps(this.dps, resp.obj);
+                        this.dialogVisible = false;
+                        //初始化变量
+                        this.initDep();
+                    }
+                })
+            },
+            showAddView(data) {
+                this.pname = data.name;
+                this.dep.parentId=data.id
+                this.dialogVisible = true;
+            },
+            deleteDep(data) {
+                if(data.parent){
+                    this.$message.error("父部门删除失败！")
+                }else{
+                    this.$confirm('是否删除选【'+ data.name +'】部门? ', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        this.deleteRequest("/system/basic/department/" + data.id).then(resp=>{
+                            if(resp){
+                                this.removeDepFromDeps(this.dps, data.id);
+                            }
+                        })
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消删除'
+                        });
+                    });
+                }
+
+            },
+            initDps() {
+                this.getRequest("/system/basic/department/").then(resp=>{
+                    if(resp){
+                        this.dps = resp;
+                    }
+                })
+            },
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.name.indexOf(value) !== -1;
+            }
+        },
     }
 </script>
 
 <style>
-
+    .depBtn {
+        padding: 2px;
+    }
 </style>
--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -21,7 +21,7 @@
         name: "SysBasic",
         data() {
             return {
-                activeName: 'permissionManage'
+                activeName: 'depManage'
             }
         },
         components: {
